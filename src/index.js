8e780063eef502509b0496147e475743852ce8ab
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-11-26 21:55:47 -0500

    Add tests for reverseString, simple calculator

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ export function capitalize(str) {
 	return str[0].toUpperCase() + str.slice(1);
 }
 
-export function reverseString(str) {}
+export function reverseString(str) {
+	return str.split('').reverse().join('');
+}
+
+export const calculator = (() => {
+	return {
+		add: (a, b) => a + b,
+		subtract: (a, b) => a - b,
+		multiply: (a, b) => a * b,
+		divide: (a, b) => a / b,
+	};
+})();
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './index';
+import { calculator, capitalize, reverseString } from './index';
 
 describe('capitalize', () => {
 	test('handles one word', () => {
@@ -6,5 +6,47 @@ describe('capitalize', () => {
 	});
 	test('handles two words', () => {
 		expect(capitalize('hello world')).toBe('Hello world');
+	});
+});
+
+describe('reverse String', () => {
+	test('handles one word', () => {
+		expect(reverseString('hello')).toBe('olleh');
+	});
+	test('handles two words', () => {
+		expect(reverseString('hello world')).toBe('dlrow olleh');
+	});
+});
+
+describe('calculator', () => {
+	test('adds 2 integers', () => {
+		expect(calculator.add(1, 2)).toBe(3);
+	});
+	test('subtracts 2 integers', () => {
+		expect(calculator.subtract(10, 3)).toBe(7);
+	});
+	test('multiplies 2 integers', () => {
+		expect(calculator.multiply(10, 3)).toBe(30);
+	});
+	test('divides 2 integers', () => {
+		expect(calculator.divide(10, 5)).toBe(2);
+	});
+});
+
+describe.skip('caesar', () => {
+	test('shifts one word', () => {
+		expect(caesarCipher(1, 2)).toBe(3);
+	});
+	test('wraps z back to a', () => {
+		expect(caesarCipher(1, 2)).toBe(3);
+	});
+	test('shifts a string of only letters', () => {
+		expect(caesarCipher(1, 2)).toBe(3);
+	});
+	test('keeps letter case the same', () => {
+		expect(caesarCipher(1, 2)).toBe(3);
+	});
+	test('ignores punctuation', () => {
+		expect(caesarCipher(1, 2)).toBe(3);
 	});
 });
