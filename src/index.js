a1a5cdf4e02b89af9fb1761c12b70916b2d33617
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-11-28 11:31:57 -0500

    Restart eslint, ignore test files

--- src/index.js
+++ src/index.js
@@ -1,45 +1,29 @@
-export function capitalize(str) {
-	return str[0].toUpperCase() + str.slice(1);
-}
+export const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
 
-export function reverseString(str) {
-	return str.split('').reverse().join('');
-}
+export const reverseString = (str) => str.split('').reverse().join('');
 
-export const calculator = (() => {
-	return {
-		add: (a, b) => a + b,
-		subtract: (a, b) => a - b,
-		multiply: (a, b) => a * b,
-		divide: (a, b) => a / b,
-	};
-})();
-
-export function caesarCipher(str, shiftFactor) {
-	const { lowerDict, upperDict } = getDictionaries(shiftFactor);
-	return str
-		.split('')
-		.map((char) => {
-			return lowerDict[char] ?? upperDict[char] ?? char;
-		})
-		.join('');
-}
+export const calculator = (() => ({
+	add: (a, b) => a + b,
+	subtract: (a, b) => a - b,
+	multiply: (a, b) => a * b,
+	divide: (a, b) => a / b,
+}))();
 
 function getDictionaries(shiftFactor) {
-	const lowerAlf = 'abcdefghijklmnopqrstuvwxyz'.split('');
-	const lowerShifted = lowerAlf.map((char, index) => {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const lowerShifted = alphabet.map((char, index) => {
 		const shiftIndex = index + shiftFactor;
-		return lowerAlf[
-			shiftIndex < lowerAlf.length ? shiftIndex : shiftIndex - lowerAlf.length
+		return alphabet[
+			shiftIndex < alphabet.length ? shiftIndex : shiftIndex - alphabet.length
 		];
 	});
 
-	let lowerDict = {};
-	lowerAlf.forEach((char, index) => {
+	const lowerDict = {};
+	alphabet.forEach((char, index) => {
 		lowerDict[char] = lowerShifted[index];
 	});
 
-	let upperDict = {};
+	const upperDict = {};
 	const upperInput = Object.keys(lowerDict).map((char) => char.toUpperCase());
 	const upperOutput = Object.values(lowerDict).map((char) => char.toUpperCase());
 	upperInput.forEach((char, index) => {
@@ -47,6 +31,14 @@ function getDictionaries(shiftFactor) {
 	});
 
 	return { lowerDict, upperDict };
+}
+
+export function caesarCipher(str, shiftFactor) {
+	const { lowerDict, upperDict } = getDictionaries(shiftFactor);
+	return str
+		.split('')
+		.map((char) => lowerDict[char] ?? upperDict[char] ?? char)
+		.join('');
 }
 
 export function analyzeArray(arr) {
